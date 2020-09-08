accda91282e5012ccb22973c5231621b3c59f45f
auddnjs2008 <46766443+auddnjs2008@users.noreply.github.com>
2020-09-08 20:58:07 +0900

    Updates

---
+++ precache-manifest.ae8cc90d169975af39e002f6c50c0ca9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4c5f11d100c81b666fd65eea56e31ac",
+    "url": "/MWflix/index.html"
+  },
+  {
+    "revision": "7796d27b14174082c0ea",
+    "url": "/MWflix/static/js/2.a44ae40d.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/MWflix/static/js/2.a44ae40d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f281d8c07aae82a35d71",
+    "url": "/MWflix/static/js/main.4c863a53.chunk.js"
+  },
+  {
+    "revision": "f2d78da67c19f13c71f9",
+    "url": "/MWflix/static/js/runtime-main.ce3fc902.js"
+  },
+  {
+    "revision": "87a0eb6a89037f2aa1b57bba7d2db7e1",
+    "url": "/MWflix/static/media/noPosterSmall.87a0eb6a.png"
+  }
+]);
